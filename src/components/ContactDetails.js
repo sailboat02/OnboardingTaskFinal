f3b014a22849d03dd6e5f7a52366fb6ec6a3da7a
sailboat02 <sailee.s@outlook.com>
2021-01-25 12:58:32 -0500

    contact street added to contact details

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -11,12 +11,12 @@ console.log(location);
     <div>
       <center><h1>Contact Details</h1></center>
         <div class="card">
-          <img src = "https://pbs.twimg.com/media/Efso_-yUwAAJpRV.jpg" width = '152px' height= '152px' alt="Avatar"></img>
+          <img src = "https://pbs.twimg.com/media/Efso_-yUwAAJpRV.jpg" width = '204px' height= '204px' alt="Avatar"></img>
             <div class="container">
               <h5 class="card-text"> phone: {user.phone}</h5>
               <h5 class="card-text"> email: {user.email}</h5>
               <h5 class="card-text"> website: {user.website}</h5>
-
+              <h5 class="card-text"> street: {user.address.street}</h5>
             </div>
         </div>
     </div>
